f616cfaa6519b57bab42c1228b6f5bf37da3afd2
tangql <360603837@qq.com>
2022-10-13 18:16:56 +0800

    fix: deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-# 确保脚本抛出遇到的错误
-set -e
+set -e # exit with nonzero exit code if anything fails
 
 # 生成静态文件
-npm run build
+yarn build
 
 # 进入生成的文件夹
 # cd docs/.vuepress/dist
@@ -14,14 +13,14 @@ cd dist
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg="deploy"
-  githubUrl="git@github.com:PathFun/vite-template-js.git"
+  githubURL="git@github.com:PathFun/vite-template-js.git"
 else
   msg="来自github actions的自动部署"
-  githubUrl="https://${GITHUB_TOKEN}@github.com:PathFun/vite-template-js.git"
+  githubURL="https://${GITHUB_TOKEN}@github.com:PathFun/vite-template-js.git"
   git config --global user.name "PathFun"
   git config --global user.email "360603837@qq.com"
 fi
 git init
 git add -f .
 git commit -m "${msg}"
-git push --force --quiet $githubUrl master:gh-pages > /dev/null # 推送到github gh-pages分支
+git push --force --quiet $githubURL master:gh-pages # 推送到github gh-pages分支
